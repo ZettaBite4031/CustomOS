366cb14d9f2bdb9a6bdf3da6d1b831777ce6bb3b
ZettaBite4031 <ddayboy123@gmail.com>
2025-09-06 19:00:06 -0400

    Added a lower case version to match normal c/c++

--- src/libs/core/Assert.hpp
+++ src/libs/core/Assert.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#define assert(cond) Assert(cond)
 #define Assert(condition) ((condition) || _Assert(#condition, __FILE__, __LINE__, __FUNCTION__))
 
 bool _Assert(const char* condition, const char* file, int line, const char* func);
